8223a1612600af50bebdc0bc8368522bbafcb952
slezkinis <slezkin.is@mail.ru>
2023-10-07 20:47:03 +0300

    Remove input()

--- data/test.py
+++ data/test.py
@@ -3,46 +3,44 @@ import multiprocessing
 
 
 def import_test():
-    import data.solution
-
-def test_function(function, params, output):
-    import data.solution
-    reload(data.solution)
-    # print(eval(f'data.solution.{str(function)[:len(function) - 1]}'))
     try:
-        eval(f'data.solution.{str(function) + str(params)}')
-    except AssertionError:
-        raise AssertionError
+        import data.solution
+    except:
+        return
 
-def test_time_function(function, params):
-    import data.solution
-    eval(f'data.solution.{str(function) + str(params)}')
+def test_function(function):
+    try:
+        import data.solution
+        reload(data.solution)
+        # print(eval(f'data.solution.{str(function)[:len(function) - 1]}'))
+        eval(f'data.solution.{function}')
+    except:
+        return
 
-def start_test(function, params, output):
+
+def start_test(function, output):
     process = multiprocessing.Process(target=import_test)
     process.start()
 
     # Ждём 300 секунд (5 минут) 
     process.join(3)
-
     # Если процесс живой,то убиваем его
     if process.is_alive():
-        print('KILL')
         process.terminate()
         raise TimeoutError
-    process2 = multiprocessing.Process(target=test_function, args=(function, params, output, ))
+    process2 = multiprocessing.Process(target=test_function, args=(function, ))
     process2.start()
     # Ждём 300 секунд (5 минут) 
-    process2.join(2)
+    process2.join(3)
 
     # Если процесс живой,то убиваем его
     if process2.is_alive():
         process2.terminate()
-        raise TimeoutError    
+        raise TimeoutError   
     import data.solution
     reload(data.solution)
     # # print(eval(f'data.solution.{str(function)[:len(function) - 1]}'))
-    assert eval(f'data.solution.{str(function) + str(params)}') == eval(output)
+    assert eval(f'data.solution.{function}') == eval(output)
     # # print(1)
 
 
@@ -51,9 +49,8 @@ def main(contest):
     tests = ContestTest.objects.filter(contest=contest)
     num_last_test = len(tests)
     for num, test in enumerate(tests, 1):
-        fun, params = test.input.split('(', 2)[0], f'({test.input.split("(", 2)[1]}'
         try:
-            start_test(fun, params, test.answer)
+            start_test(test.input, test.answer)
         except AssertionError:
             return (f'Error! Incorrect answer!', f'Test {num}/{num_last_test}')
         except TimeoutError:
